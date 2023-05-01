7ad0146d5fe29e6bc376237f5455c96f13f6fc57
jvpoly <54869526+jvpoly@users.noreply.github.com>
2023-05-01 11:27:46 -0700

    first commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import * as variables from "../styles/variables";
+
+// import cv from "/src/assets/cv/JaimeVillanuaDeJuan.pdf";
+
+function NavBar() {
+  
+
+  return (
+    <></>
+  );
+}
---
+++ src/styles/variables.tsx
@@ -0,0 +1,11 @@
+export const style = {
+    backgroundOne: "#5a0b52",
+    backgroundTwo:"#304B38",
+    backgroundThree:"#cfcfcf",
+    primaryOne:"#f5f5f5",
+    primaryTwo:"#D4499A",
+    primaryThree:"#2b2b2b",
+    tabletWidth: "768px",
+    desktopWidth: "1280px",
+    gradient: "linear-gradient(45deg, #2f864b 0%, #1e77aa 49%, #9c2390 100%)"
+}
