487f24a66f6527ff469f0d4d50ee924c38ac6879
jvpoly <54869526+jvpoly@users.noreply.github.com>
2023-05-08 14:41:56 -0700

    logo socials changed in desktop view and description added to projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -12,6 +12,18 @@ const Projects = () => {
       <ul className="projects__list">
         <li className="projects__item">
           <h3 className="projects__item-title">JAMES V</h3>
+          <p className="projects__item-description">
+            JAMES V is a Full Stack application made in collaboration with
+            another Full Stack Developer for the Vancouver based artist{" "}
+            <a className="link" href="https://jamesvmusic.com/">
+              {" "}
+              JAMES V
+            </a>
+            <br></br>To build an outstading interface, we used ReactJS for the
+            Front End. For the BackEnd we used FireBase for hosting the web
+            app's database and domain and creating cloud functions to retrieve
+            data automatically daily for the videos section.
+          </p>
           <img
             src={jamesvCover}
             className="projects__item-cover"
@@ -27,6 +39,14 @@ const Projects = () => {
         </li>
         <li className="projects__item">
           <h3 className="projects__item-title">RaveFinder</h3>
+          <p className="projects__item-description">
+            RaveFinder application is a promotional searvh engine where users
+            can look for concerts where their favorite artists are playing, find
+            a safe purchase link to buy the ticket for that event and find out
+            about other upcoming events. This is a Front End application made in
+            ReactJS that utilizes TicketMaster's API to retrieve all the
+            information about the events.
+          </p>
           <img
             className="projects__item-cover"
             src={ravefinderCover}
@@ -47,6 +67,13 @@ const Projects = () => {
         </li>
         <li className="projects__item">
           <h3 className="projects__item-title">BrainFlix</h3>
+          <p className="projects__item-description">
+            BrainFlix is a video streaming website similar in design and
+            functionality to Vimeo. The tech stack of this project includes
+            ReactJs for the Front End and ExpressJs for the Back End API. The
+            user can select and view videos from a suggestion bar as well as
+            upload videos
+          </p>
           <img
             src={brainflixCover}
             className="projects__item-cover"
